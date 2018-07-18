985015c3a66306735ba3dfa714a158a5faad9648
Faustino Vasquez Limon <vasquez.limon@gmail.com>
2018-07-17 17:40:18 -0700

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.auth')
+@section('title')
+    Login
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card p-4">
+                    <div class="card-header text-center text-uppercase h4 font-weight-light">
+                        Login
+                    </div>
+                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                        @csrf
+                        <div class="card-body py-5">
+                            <div class="form-group">
+                                <label class="form-control-label">Email</label>
+                                <input name="email"
+                                       type="email"
+                                       class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                       value="{{ old('email') }}">
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+
+                            <div class="form-group">
+                                <label class="form-control-label">Password</label>
+                                <input name="password"
+                                       type="password"
+                                       class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}">
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+
+                            <div class="custom-control custom-checkbox mt-4">
+                                <input type="checkbox"
+                                       class="custom-control-input"
+                                       id="login"
+                                       name="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <label class="custom-control-label" for="login">Remember Me</label>
+                            </div>
+                        </div>
+
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-6">
+                                    <button type="submit" class="btn btn-primary px-5">Login</button>
+                                </div>
+
+                                <div class="col-6">
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}</a>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
