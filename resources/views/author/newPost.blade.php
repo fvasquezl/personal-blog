72f6fbf155c6664aad8da39a17f9fa6ad8ba4f01
Faustino Vasquez Limon <vasquez.limon@gmail.com>
2018-07-20 17:44:41 -0700

    Create Post not workig

---
+++ resources/views/author/newPost.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.admin')
+
+@section('title') New Post @endsection
+
+@section('content')
+<div class="content">
+    <form action="">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header bg-light">
+                            New Post
+                        </div>
+
+                        @if(Session::has('success'))
+                            <div class="alert alert-success">
+                                {{Session::get('success')}}
+                            </div>
+                        @endif
+                        @if($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach($errors->all() as $error)
+                                        <li>{{$error}}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        <form action="{{route('createPost')}}" method="POST">
+                            @csrf
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <div class="form-group">
+                                            <label for="normal-input" class="form-control-label">Title</label>
+                                            <input name="title" id="normal-input" class="form-control" placeholder="Post Title">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row mt-4">
+                                    <div class="col-md-8">
+                                        <div class="form-group">
+                                            <label for="placeholder-input" class="form-control-label">Content</label>
+                                            <textarea  placeholder="Describe yourself here..."
+                                                       name="content"
+                                                      cols="30"
+                                                      rows="10"
+                                                      class="form-control"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button class="btn btn-success" type="submit">Create Post</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+@endsection
